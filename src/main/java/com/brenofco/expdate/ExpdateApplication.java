ee507dd0427e51c4de32ba32004e5cb2cd143f4f
brenofco <brenofco@gmail.com>
2024-02-08 23:45:35 -0300

    Initial commit

---
+++ src/main/java/com/brenofco/expdate/ExpdateApplication.java
@@ -0,0 +1,13 @@
+package com.brenofco.expdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpdateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brenofco/expdate/ExpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenofco.expdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
